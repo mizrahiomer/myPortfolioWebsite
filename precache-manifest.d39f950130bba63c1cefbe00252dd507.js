fa891cd2f34be45c21b15fc192254581d900157d
Omer <omermiz1604gmail.com>
2020-01-21 14:49:15 +0200

    Updates

---
+++ precache-manifest.d39f950130bba63c1cefbe00252dd507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae172dcc491960a0531d7b779b4c4cd6",
+    "url": "/myPortfolioWebsite/index.html"
+  },
+  {
+    "revision": "94961291817d6344de4a",
+    "url": "/myPortfolioWebsite/static/css/main.dcd5ca1a.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/myPortfolioWebsite/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myPortfolioWebsite/static/js/2.f175dcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94961291817d6344de4a",
+    "url": "/myPortfolioWebsite/static/js/main.04319c33.chunk.js"
+  },
+  {
+    "revision": "5041ca90fb1e0e261fa6",
+    "url": "/myPortfolioWebsite/static/js/runtime-main.b8f1e593.js"
+  }
+]);
