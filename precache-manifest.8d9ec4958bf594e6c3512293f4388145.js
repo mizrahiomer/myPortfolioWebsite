c304616db9e205ac7097b331030e060b67013d0b
mizrahiomer <omermiz1604@gmail.com>
2020-04-21 15:40:19 +0300

    Updates

---
+++ precache-manifest.8d9ec4958bf594e6c3512293f4388145.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c572f341bc25c93195fe7fab063029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590d3c89f3afd2ac6518",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8759f8d9dfeb4519b6cc",
+    "url": "/static/css/main.a9325308.chunk.css"
+  },
+  {
+    "revision": "590d3c89f3afd2ac6518",
+    "url": "/static/js/2.458557e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.458557e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8759f8d9dfeb4519b6cc",
+    "url": "/static/js/main.d95f7dc0.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "ecf9c844dffe5ef2bb90073c58c7961c",
+    "url": "/static/media/myImg.ecf9c844.JPEG"
+  },
+  {
+    "revision": "4b68c5fc85185e68209506f73b207421",
+    "url": "/static/media/nba.4b68c5fc.gif"
+  },
+  {
+    "revision": "be509b8eeca76a3147a99d1c96d77e33",
+    "url": "/static/media/nba.be509b8e.mp4"
+  },
+  {
+    "revision": "58df8b3b4c418db8bf17c51f68c4405b",
+    "url": "/static/media/saida.58df8b3b.mp4"
+  },
+  {
+    "revision": "844e3529e9edd8c82c0f582d10c9e633",
+    "url": "/static/media/saida.844e3529.gif"
+  },
+  {
+    "revision": "02053bbbac7c58720bc2b1b524222544",
+    "url": "/static/media/tictactoe.02053bbb.gif"
+  },
+  {
+    "revision": "feed4bdc9b214a9b8711aff845796911",
+    "url": "/static/media/tictactoe.feed4bdc.mp4"
+  },
+  {
+    "revision": "0c6f2d77e6da65d7aba50b99815b5c66",
+    "url": "/static/media/todolist.0c6f2d77.mp4"
+  },
+  {
+    "revision": "40583b10d74ceaea115607abc385a928",
+    "url": "/static/media/todolist.40583b10.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a9b35087482a42fb5b888817770d256.js"
+  "/precache-manifest.8d9ec4958bf594e6c3512293f4388145.js"
 );
 
 self.addEventListener('message', (event) => {
